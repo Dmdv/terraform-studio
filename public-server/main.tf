d0e9a43c34b38f8cc6b4340a6f630945556c31fc
DmitryD <dima.dyachkov@gmail.com>
2023-10-16 14:30:08 +0800

    Removed AWS config

--- public-server/main.tf
+++ public-server/main.tf
@@ -1,12 +1,9 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAJTTSSUF2PB6HDCCA"
-  secret_key = "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"
 }
 
 variable "subnet_prefix" {
   description = "cidr block for the subnet"
-
 }
 
 resource "aws_vpc" "prod-vpc" {
