9b98cb00513a68f2efe05231ec6538fbdfe000ee
DmitryD <dima.dyachkov@gmail.com>
2023-10-17 12:02:00 +0800

    Security group

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,3 +1,4 @@
+# Information about the AMI
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,11 +15,59 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# Information about the VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create an instance
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  # Associate the instance with the VPC
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# Create a security group
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow HTTP and HTTPS inbound traffic. And allow all outbound traffic."
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Create a security group rule
+resource "aws_security_group_rule" "blog_http_in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
