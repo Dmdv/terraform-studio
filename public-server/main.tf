6ab690520c3516732affa41fd22dbad9f0c62e2f
DmitryD <dima.dyachkov@gmail.com>
2023-10-17 12:01:50 +0800

    Configuring route table and router

--- public-server/main.tf
+++ public-server/main.tf
@@ -2,6 +2,8 @@ variable "subnet_prefix" {
   description = "cidr block for the subnet"
 }
 
+# 1. Create vpc
+
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -9,15 +11,34 @@ resource "aws_vpc" "prod-vpc" {
   }
 }
 
-# # 1. Create vpc
+# 2. Create Internet Gateway
 
-# resource "aws_vpc" "prod-vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     Name = "production"
-#   }
-# }
+resource "aws_internet_gateway" "gw" {
+   vpc_id = aws_vpc.prod-vpc.id
+}
 
+# 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+   vpc_id = aws_vpc.prod-vpc.id
+
+   route {
+     cidr_block = "0.0.0.0/0" # all ipv4 traffic
+     gateway_id = aws_internet_gateway.gw.id # to be routed to the internet gateway
+   }
+
+   route {
+     ipv6_cidr_block        = "::/0" # all ipv6 traffic
+     gateway_id             = aws_internet_gateway.gw.id
+     # egress_only_gateway_id = aws_internet_gateway.gw.id
+   }
+
+   tags = {
+     Name = "Prod"
+   }
+}
+
+# 4. Create a Subnet
 
 resource "aws_subnet" "subnet-1" {
   vpc_id            = aws_vpc.prod-vpc.id
@@ -39,90 +60,63 @@ resource "aws_subnet" "subnet-2" {
   }
 }
 
+resource "aws_subnet" "subnet-1" {
+   vpc_id            = aws_vpc.prod-vpc.id
+   cidr_block        = "10.0.1.0/24"
+   availability_zone = "us-east-1a"
 
-# # 2. Create Internet Gateway
+   tags = {
+     Name = "prod-subnet"
+   }
+}
 
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.prod-vpc.id
+# 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+ subnet_id      = aws_subnet.subnet-1.id
+ route_table_id = aws_route_table.prod-route-table.id
+}
 
+ # 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+ name        = "allow_web_traffic"
+ description = "Allow Web inbound traffic"
+ vpc_id      = aws_vpc.prod-vpc.id
 
-# }
-# # 3. Create Custom Route Table
+ ingress {
+   description = "HTTPS"
+   from_port   = 443
+   to_port     = 443
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
-# resource "aws_route_table" "prod-route-table" {
-#   vpc_id = aws_vpc.prod-vpc.id
+ ingress {
+   description = "HTTP"
+   from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
+ ingress {
+   description = "SSH"
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
-#   route {
-#     ipv6_cidr_block = "::/0"
-#     gateway_id      = aws_internet_gateway.gw.id
-#   }
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
-#   tags = {
-#     Name = "Prod"
-#   }
-# }
-
-# # 4. Create a Subnet 
-
-# resource "aws_subnet" "subnet-1" {
-#   vpc_id            = aws_vpc.prod-vpc.id
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = "us-east-1a"
-
-#   tags = {
-#     Name = "prod-subnet"
-#   }
-# }
-
-# # 5. Associate subnet with Route Table
-# resource "aws_route_table_association" "a" {
-#   subnet_id      = aws_subnet.subnet-1.id
-#   route_table_id = aws_route_table.prod-route-table.id
-# }
-# # 6. Create Security Group to allow port 22,80,443
-# resource "aws_security_group" "allow_web" {
-#   name        = "allow_web_traffic"
-#   description = "Allow Web inbound traffic"
-#   vpc_id      = aws_vpc.prod-vpc.id
-
-#   ingress {
-#     description = "HTTPS"
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     description = "HTTP"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     description = "SSH"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "allow_web"
-#   }
-# }
+ tags = {
+   Name = "allow_web"
+ }
+}
 
 # # 7. Create a network interface with an ip in the subnet that was created in step 4
 
