2569ac6272706e5bfb5fbdff8c50892577ed5050
DmitryD <dima.dyachkov@gmail.com>
2023-10-20 12:09:38 +0800

    varaibles

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -12,7 +12,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 # Default VPC security group
@@ -40,10 +40,10 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name = "${var.environment.name}-blog-autoscaling"
 
-  min_size            = 1
-  max_size            = 2
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_security_group.security_group_id]
@@ -56,7 +56,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -66,7 +66,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -82,7 +82,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -90,11 +90,14 @@ module "blog_alb" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev blog VPC"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["ap-east-1a", "ap-east-1b", "ap-east-1c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = [
+    "${var.environment.network_prefix}.101.0/24",
+    "${var.environment.network_prefix}.102.0/24",
+    "${var.environment.network_prefix}.103.0/24"]
 
   # Uncomment this to create a NAT gateway
   #  enable_nat_gateway = true
@@ -102,7 +105,7 @@ module "blog_vpc" {
 
   tags = {
     Terraform = true,
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -110,7 +113,7 @@ module "blog_vpc" {
 module "blog_security_group" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "blog security group"
+  name        = "${var.environment.name}-blog-security-group}"
   description = "Allow HTTP and HTTPS inbound traffic. And allow all outbound traffic."
 
   vpc_id      = module.blog_vpc.vpc_id
