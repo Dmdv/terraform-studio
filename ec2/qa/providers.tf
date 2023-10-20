5988ff09a2c2399be35271f0774012b0380ef35a
DmitryD <dima.dyachkov@gmail.com>
2023-10-20 13:29:00 +0800

    Added QA and Prod env

---
+++ ec2/qa/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+#  backend "s3" {
+#    bucket         = "learning.terraform.tf-state.root.ap-east-1"
+#    key            = "aws/root/s3/terraform.tfstate"
+#    region         = "ap-east-1"
+#    encrypt        = true
+#  }
+}
+
+provider "aws" {
+  alias  = "hk"
+  region  = "ap-east-1"
+}
